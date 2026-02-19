6b608867e38405c1f825e6d872053053767934eb
Rajat <light.rajat+gh@gmail.com>
2026-02-19 23:16:12 +0530

    feat: Added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const maxWidth = '940px';
+const navLinks = [
+    { link: '/', name: 'Home' },
+    { link: '/recorder', name: 'Recorder' },
+    { link: '/dashboard', name: 'Dashboard' },
+];
+</script>
+
+<template>
+    <div class="nav-container">
+        <nav :style="`max-width: ${maxWidth}`" class="d-flex justify-content-center mx-auto lead">
+            <RouterLink v-for="(link, index) in navLinks" :key="index" :to="link.link" class="nav-link p-2" :class="route.path == link.link ? 'fw-medium' : ''">
+                <i v-if="index === 0" class="bi bi-house"></i>
+                {{ link.name }}
+            </RouterLink>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+.nav-container {
+    width: 100%;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import Skeleton from '@/components/Skeleton.vue';
 import Home from '@/views/Home.vue';
 import Recorder from '@/views/Recorder.vue';
 import Dashboard from '@/views/Dashboard.vue';
@@ -7,9 +8,13 @@ import Dashboard from '@/views/Dashboard.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', name: 'home', component: Home },
-        { path: '/recorder', name: 'recorder', component: Recorder },
-        { path: '/dashboard', name: 'dashboard', component: Dashboard },
+        {
+            path: '/', name: 'skeleton', component: Skeleton, children: [
+                { path: '/', name: 'home', component: Home },
+                { path: '/recorder', name: 'recorder', component: Recorder },
+                { path: '/dashboard', name: 'dashboard', component: Dashboard },
+            ]
+        }
     ]
 });
 
