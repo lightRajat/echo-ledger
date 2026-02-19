325303e05df34b8cd4cc20c4c59bc6608eb7bbb3
Rajat <light.rajat+gh@gmail.com>
2026-02-19 21:20:25 +0530

    feat: Added router

---
+++ src/router.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Home from '@/views/Home.vue';
+import Recorder from '@/views/Recorder.vue';
+import Dashboard from '@/views/Dashboard.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', name: 'home', component: Home },
+        { path: '/recorder', name: 'recorder', component: Recorder },
+        { path: '/dashboard', name: 'dashboard', component: Dashboard },
+    ]
+});
+
+router.afterEach((to) => {
+    document.title = to.meta.title || "Echo Ledger";
+});
+
+export default router;
---
+++ src/views/Recorder.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Recorder</h1>
+</template>
