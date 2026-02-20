493153b8a84a6b7030c0a357fc0c78329679e7b6
Rajat <light.rajat+gh@gmail.com>
2026-02-20 17:55:49 +0530

    feat: Integrated silero vad

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,28 @@
+from const import SAMPLE_RATE
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from silero_vad import load_silero_vad
+import torch
 import uvicorn
+import warnings
+warnings.filterwarnings("ignore")
 
 app = FastAPI()
+vad_model = load_silero_vad()
 
 @app.websocket("/")
 async def home(websocket: WebSocket):
     await websocket.accept()
-    print("Client connected")
     
     try:
         while True:
             websocket_bytes = await websocket.receive_bytes()
-            print(f"Received audio chunk: {len(websocket_bytes)} bytes")
+            torch_tensor = torch.frombuffer(websocket_bytes, dtype=torch.float32)
+            speech_prob = vad_model(torch_tensor, SAMPLE_RATE).item()
+            if speech_prob > 0.5:
+                print("Speech detected")
 
     except WebSocketDisconnect:
-        print("Client disconnected")
+        pass
     except Exception as e:
         print(f"Error: {e}")
 
