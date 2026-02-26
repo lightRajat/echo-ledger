091888e92fb83c2a776e9aed84921d495aeb6382
Rajat <light.rajat+gh@gmail.com>
2026-02-26 20:29:24 +0530

    chore: Added sample data in database

--- backend/init.py
+++ backend/init.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import sqlite3
 from huggingface_hub import hf_hub_download
@@ -8,7 +9,7 @@ from faster_whisper import download_model
 def log(message):
     print(f"[INFO]: {message}")
 
-def init_db(db_name="data.db"):
+def init_db(db_name="data/data.db"):
     if os.path.exists(db_name):
         log(f"Database '{db_name}' already exists. Skipping initialization.")
         return
@@ -52,6 +53,7 @@ def init_db(db_name="data.db"):
             s_id INTEGER,
             p_name TEXT NOT NULL,
             p_price INTEGER NOT NULL,
+            p_qty INTEGER NOT NULL,
             FOREIGN KEY (s_id) REFERENCES sale (id) ON DELETE CASCADE
         )
     ''')
@@ -59,6 +61,42 @@ def init_db(db_name="data.db"):
     conn.commit()
     conn.close()
     log(f"Database '{db_name}' initialized successfully.")
+
+def load_csv_data(db_name="data/data.db", root_folder="data/sample-data"):
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    table_mappings = {
+        f"{root_folder}/product.csv": "INSERT INTO product (name, price) VALUES (?, ?)",
+        f"{root_folder}/stock.csv": "INSERT INTO stock (p_id, qty) VALUES (?, ?)",
+        f"{root_folder}/sale.csv": "INSERT INTO sale (date) VALUES (?)",
+        f"{root_folder}/sale_items.csv": "INSERT INTO sale_items (s_id, p_name, p_price, p_qty) VALUES (?, ?, ?, ?)"
+    }
+
+    try:
+        for filename, query in table_mappings.items():
+            
+            with open(filename, mode='r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                next(reader)  # Skip the header row
+                
+                for row in reader:
+                    cursor.execute(query, row)
+                    
+        conn.commit()
+        log("All sample data added to the database")
+
+    except FileNotFoundError as e:
+        print(f"❌ Error: Could not find the file - {e.filename}")
+        conn.rollback()
+    except sqlite3.IntegrityError as e:
+        print(f"❌ Foreign Key / Integrity error: {e}")
+        conn.rollback()
+    except sqlite3.Error as e:
+        print(f"❌ Database error: {e}")
+        conn.rollback()
+    finally:
+        conn.close()
 
 def download_llama_model(repo_id: str, filename: str):
     model_path = hf_hub_download(
@@ -79,6 +117,7 @@ if __name__ == "__main__":
     load_dotenv()
 
     init_db()
+    load_csv_data()
     download_silero_model()
     downlad_whisper_model()
     download_llama_model(os.getenv("LLAMA_REPO_ID"), os.getenv("LLAMA_FILE_NAME"))
